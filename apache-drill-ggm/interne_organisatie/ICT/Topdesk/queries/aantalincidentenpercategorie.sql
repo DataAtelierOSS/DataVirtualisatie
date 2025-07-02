501c424b1d9f988b32435955340f51ae45a8a251
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-07-02 13:27:03 +0200

    Update aantalincidentenpercategorie.sql

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/queries/aantalincidentenpercategorie.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/queries/aantalincidentenpercategorie.sql
@@ -1,11 +1,6 @@
 SELECT
-  COALESCE(F.name, 'Onbekende categorie') AS categorie,  
-  COUNT(*) AS aantal_incidenten                      
-FROM topdesk.`Incidents` A
-LEFT JOIN topdesk.`Categories` F 
-    ON CAST(A.categoryId AS VARCHAR) = F.id         
-WHERE A.creationDate IS NOT NULL                     
-  AND CAST(SUBSTR(A.creationDate, 1, 4) AS INT) >= 
-      CAST(SUBSTR(CURRENT_DATE, 1, 4) AS INT) - 3     
-GROUP BY COALESCE(F.name, 'Onbekende categorie')
+  categorie,
+  COUNT(*) AS aantal_incidenten
+FROM cte_basis
+GROUP BY categorie
 ORDER BY aantal_incidenten DESC;

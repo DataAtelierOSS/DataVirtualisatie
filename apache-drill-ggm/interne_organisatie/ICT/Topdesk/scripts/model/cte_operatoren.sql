b4785545abd60a2abc22e9d81a0ad5cc637c05c5
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-07-21 10:49:48 +0200

    Update cte_operatoren.sql

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_operatoren.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_operatoren.sql
@@ -1,9 +1,12 @@
 CREATE TABLE dfs.tmp.cte_operatoren AS
-SELECT 
-  CAST(op.id AS VARCHAR) AS operator_id,
-  op.name AS operator_name,
-  CAST(og.id AS VARCHAR) AS operatorgroup_id,
-  og.name AS operatorgroup_name
-FROM topdesk.topdesk.`Operators` op
-LEFT JOIN topdesk.topdesk.`OperatorGroups` og
-  ON CAST(op.operatorGroupId AS VARCHAR) = CAST(og.id AS VARCHAR);
+SELECT
+  DISTINCT
+  CAST(A.operatorid AS VARCHAR) AS operator_id,
+  F.name AS operator_name,
+  CAST(A.operatorGroupid AS VARCHAR) AS operatorgroup_id,
+  E.name AS operatorgroup_name
+FROM topdesk.topdesk.`Incidents` A
+LEFT JOIN topdesk.topdesk.`OperatorGroups` E ON CAST(A.operatorGroupid AS VARCHAR) = E.id
+LEFT JOIN topdesk.topdesk.`Operators` F ON CAST(A.operatorid AS VARCHAR) = F.id
+WHERE A.operatorid IS NOT NULL
+   OR A.operatorGroupid IS NOT NULL;

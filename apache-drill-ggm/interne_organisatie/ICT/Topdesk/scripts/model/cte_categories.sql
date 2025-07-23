97e72c86aa67e13847901b4609d46381e6332a3e
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-07-23 09:44:22 +0200

    Update cte_categories.sql

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_categories.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_categories.sql
@@ -2,8 +2,8 @@ CREATE TABLE dfs.tmp.cte_categories AS
 SELECT 
   CAST(c.id AS VARCHAR) AS category_id,
   c.name AS category_name,
-  CAST(s.id AS VARCHAR) AS subcategory_id,
+  CAST(s.mainCategoryId AS VARCHAR) AS subcategory_id,
   s.name AS subcategory_name
 FROM topdesk.topdesk.`Categories` c
 LEFT JOIN topdesk.topdesk.`Subcategories` s
-  ON CAST(s.categoryId AS VARCHAR) = CAST(c.id AS VARCHAR);
+  ON CAST(s.mainCategoryId AS VARCHAR) = CAST(c.id AS VARCHAR);

ebc3e8fa92c3854748f7492369d5038f13acb88f
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-08-04 13:12:16 +0200

    Update cte_suppliers.sql
    duplicates eruit gehaald.

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_suppliers.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_suppliers.sql
@@ -1,8 +1,19 @@
 CREATE TABLE dfs.tmp.cte_suppliers AS
 SELECT 
-  CAST(s.id AS VARCHAR) AS supplier_id,
-  s.name AS supplier_name,
-  CAST(sc.operatorId AS VARCHAR) AS operator_id
-FROM topdesk.topdesk.`Suppliers` s
-JOIN topdesk.topdesk.`SupplierContracts` sc
-  ON CAST(s.id AS VARCHAR) = CAST(sc.supplierId AS VARCHAR);
+    supplier_id,
+    supplier_name,
+    operator_id
+FROM (
+    SELECT 
+        CAST(s.id AS VARCHAR) AS supplier_id,
+        s.name AS supplier_name,
+        CAST(sc.operatorId AS VARCHAR) AS operator_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY s.id, sc.operatorId 
+            ORDER BY s.name
+        ) AS rn
+    FROM topdesk.topdesk.`Suppliers` s
+    JOIN topdesk.topdesk.`SupplierContracts` sc
+        ON CAST(s.id AS VARCHAR) = CAST(sc.supplierId AS VARCHAR)
+) t
+WHERE rn = 1;

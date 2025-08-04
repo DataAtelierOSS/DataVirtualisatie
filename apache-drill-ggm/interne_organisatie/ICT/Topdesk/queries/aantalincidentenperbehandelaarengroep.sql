f6ae0b0d6acaa04afa520f920ebd3f4cb3524bd1
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-08-04 14:49:16 +0200

    Update aantalincidentenperbehandelaarengroep.sql

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/queries/aantalincidentenperbehandelaarengroep.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/queries/aantalincidentenperbehandelaarengroep.sql
@@ -1,9 +1,21 @@
 --dfs.tmp.incidenten_basis_v2 voor het model moet je de from hierin veranderen!
 
-SELECT
-  behandelaarsgroep,
-  behandelaar,
-  COUNT(*) AS aantal_incidenten
-FROM cte_basis
+SELECT 
+    behandelaarsgroep,
+    behandelaar,
+    COUNT(*) AS aantal_incidenten
+FROM dfs.tmp.incidenten_basis_v2
+WHERE CAST(
+        REPLACE(
+            REPLACE(SUBSTR(aanmelddatum, 1, 19), 'T', ' ')
+        , 'Z', ''
+        ) AS TIMESTAMP
+    ) >= TIMESTAMP '2025-03-01 00:00:00'
+  AND CAST(
+        REPLACE(
+            REPLACE(SUBSTR(aanmelddatum, 1, 19), 'T', ' ')
+        , 'Z', ''
+        ) AS TIMESTAMP
+    ) < TIMESTAMP '2025-04-01 00:00:00'
 GROUP BY behandelaarsgroep, behandelaar
-ORDER BY behandelaarsgroep, aantal_incidenten DESC;
+ORDER BY aantal_incidenten DESC;

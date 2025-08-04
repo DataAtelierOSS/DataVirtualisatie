c230b855e464da00748bfb79e6b81c3727a5504f
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-08-04 14:10:23 +0200

    Update cte_incidenttypes.sql

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_incidenttypes.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/model/cte_incidenttypes.sql
@@ -1,11 +1,5 @@
 CREATE TABLE dfs.tmp.cte_incidenttypes AS
 SELECT 
-  CAST(d.id AS VARCHAR) AS incident_id,
-  d.incidentnumber,
-  d.briefDescription,
-  CAST(d.assetid AS VARCHAR) AS assetid,
   CAST(t.id AS VARCHAR) AS incidenttype_id,
   t.name AS incidenttype_name
-FROM topdesk.topdesk.`IncidentDetails` d
-LEFT JOIN topdesk.topdesk.`IncidentTypes` t
-  ON CAST(d.typeId AS VARCHAR) = CAST(t.id AS VARCHAR);
+FROM topdesk.topdesk.`IncidentTypes` t;

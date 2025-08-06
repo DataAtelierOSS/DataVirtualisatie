75958d177249178a546a71a9f068308bc898454d
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-08-06 14:26:22 +0200

    Update applicaties.sql

--- apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/views/applicaties.sql
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/views/applicaties.sql
@@ -1,1 +1,49 @@
 
+CREATE VIEW dfs.tmp.vw_cte_applicaties AS
+WITH
+  filtered_software AS (
+    SELECT
+      CAST(id AS VARCHAR) AS id,
+      CAST(name AS VARCHAR) AS name,
+      CAST(objectid AS VARCHAR) AS objectid,
+      CAST(REPLACE(SUBSTR(creationDate, 1, 19), 'T', ' ') AS TIMESTAMP) AS parsed_creationDate
+    FROM topdesk.topdesk.`AssetSoftwareList`
+    WHERE CAST(REPLACE(SUBSTR(creationDate, 1, 19), 'T', ' ') AS TIMESTAMP) >= TIMESTAMP '2025-01-01 00:00:00'
+  ),
+
+  filtered_detail AS (
+    SELECT
+      CAST(id AS VARCHAR) AS id,
+      CAST(application AS VARCHAR) AS application
+    FROM topdesk.topdesk.`AssetSoftwareDetailList`
+  ),
+
+  filtered_server AS (
+    SELECT
+      CAST(id AS VARCHAR) AS id,
+      CAST(objectid AS VARCHAR) AS objectid,
+      CAST(REPLACE(SUBSTR(creationDate, 1, 19), 'T', ' ') AS TIMESTAMP) AS parsed_creationDate
+    FROM topdesk.topdesk.`AssetServerList`
+    WHERE CAST(REPLACE(SUBSTR(creationDate, 1, 19), 'T', ' ') AS TIMESTAMP) >= TIMESTAMP '2025-01-01 00:00:00'
+  )
+
+SELECT 
+  s.id,
+  MIN(
+    COALESCE(
+      d.application,
+      s.name,
+      s.objectid
+    )
+  ) AS applicatie
+FROM filtered_software s
+LEFT JOIN filtered_detail d
+  ON s.id = d.id
+GROUP BY s.id
+
+UNION ALL
+
+SELECT
+  id,
+  objectid AS applicatie
+FROM filtered_server;

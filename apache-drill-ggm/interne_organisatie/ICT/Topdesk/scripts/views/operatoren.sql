c5fff66b76300fe37697d9dd438013267f260975
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-08-06 14:30:05 +0200

    Create operatoren.sql

---
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/views/operatoren.sql
@@ -0,0 +1,13 @@
+CREATE VIEW dfs.tmp.vw_cte_operatoren AS
+SELECT DISTINCT
+  CAST(A.`operatorid` AS VARCHAR) AS operator_id,
+  F.name AS operator_name,
+  CAST(A.`operatorGroupid` AS VARCHAR) AS operatorgroup_id,
+  E.name AS operatorgroup_name
+FROM topdesk.topdesk.`Incidents` A
+LEFT JOIN topdesk.topdesk.`OperatorGroups` E
+  ON CAST(A.`operatorGroupid` AS VARCHAR) = CAST(E.id AS VARCHAR)
+LEFT JOIN topdesk.topdesk.`Operators` F
+  ON CAST(A.`operatorid` AS VARCHAR) = CAST(F.id AS VARCHAR)
+WHERE A.`operatorid` IS NOT NULL
+   OR A.`operatorGroupid` IS NOT NULL;

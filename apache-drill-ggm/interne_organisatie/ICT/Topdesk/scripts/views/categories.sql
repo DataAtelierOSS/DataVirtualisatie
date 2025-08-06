c34486210e02572602853e201d4ae4263487d953
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-08-06 14:26:53 +0200

    Create categories.sql

---
+++ apache-drill-ggm/interne_organisatie/ICT/Topdesk/scripts/views/categories.sql
@@ -0,0 +1,9 @@
+CREATE VIEW dfs.tmp.vw_cte_categories AS
+SELECT 
+  CAST(c.id AS VARCHAR) AS category_id,
+  c.name AS category_name,
+  CAST(s.id AS VARCHAR) AS subcategory_id,
+  s.name AS subcategory_name
+FROM topdesk.topdesk.`Categories` c
+LEFT JOIN topdesk.topdesk.`Subcategories` s
+  ON CAST(s.mainCategoryId AS VARCHAR) = CAST(c.id AS VARCHAR);

50c7ff01f93b650da81c517f93830c1f29bf6ce0
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-09-15 11:50:10 +0200

    Update WSOpdracht2.sql

--- apache-drill-ggm/onderwijs/cbs/WSOpdracht2.sql
+++ apache-drill-ggm/onderwijs/cbs/WSOpdracht2.sql
@@ -5,6 +5,6 @@ SELECT
   Leerlingen_1,
   VoortijdigSchoolverlaters_2,
   VoortijdigSchoolverlatersPercentage_4
-FROM cbs.cbs.`84780NED`
+FROM cbs.cbs.`84780NED/TypedDataSet`
 WHERE RTRIM(Regiokenmerken) = 'GM1883'
 ORDER BY Perioden DESC;

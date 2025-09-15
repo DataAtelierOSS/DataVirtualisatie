06d94edb871b74d649d211cd02c4ef10586dda59
samuelvanwijhe <148537259+samuelvanwijhe@users.noreply.github.com>
2025-09-15 11:50:43 +0200

    Update WSOpdracht3.sql

--- apache-drill-ggm/onderwijs/cbs/WSOpdracht3.sql
+++ apache-drill-ggm/onderwijs/cbs/WSOpdracht3.sql
@@ -1,6 +1,6 @@
 SELECT 
   Perioden,
-  VoortijdigSchoolverlatersPercentage_4 AS perc_vo
+  VoortijdigSchoolverlatersPercentage_4 AS perc_vsv
 FROM cbs.cbs.`84780NED/TypedDataSet`
 WHERE RTRIM(Regiokenmerken) = 'GM1883'
   AND RTRIM(Geslacht) = 'T001038'
